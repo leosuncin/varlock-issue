abbb315d61c8c12a886a513006748333676403b9
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2025-11-13 17:00:27 -0600

    Add tests which print the environment variables

--- test.spec.ts
+++ test.spec.ts
@@ -1,12 +1,38 @@
-import { test } from 'node:test';
+import { test, type TestContext } from 'node:test';
 
 import request from 'supertest';
 import { ENV } from 'varlock/env';
 
 import { server } from './server.ts';
 
-test('http server test', async () => {
-  await request(server)
+test('print process.env', (context: TestContext) => {
+  console.table({
+    host: process.env.HOST,
+    port: Number(process.env.PORT),
+    env: String(process.env.NODE_ENV),
+    ci: Boolean(process.env.CI === 'true'),
+  });
+
+  context.assert.ok(process.env.HOST);
+  context.assert.ok(process.env.PORT);
+  context.assert.ok(process.env.NODE_ENV);
+});
+
+test('print ENV variables', (context: TestContext) => {
+  console.table({
+    host: ENV.HOST,
+    port: ENV.PORT,
+    env: ENV.NODE_ENV,
+    ci: ENV.CI,
+  });
+
+  context.assert.ok(ENV.HOST);
+  context.assert.ok(ENV.PORT);
+  context.assert.ok(ENV.NODE_ENV);
+});
+
+test('http server test', async (context: TestContext) => {
+  const response = await request(server)
     .get('/')
     .expect(200)
     .expect('Content-Type', 'application/json')
@@ -16,4 +42,6 @@ test('http server test', async () => {
       env: String(ENV.NODE_ENV),
       ci: Boolean(ENV.CI),
     });
+
+  context.assert.ok(response);
 });
